9c8f38c3f42806fe190844dc9be950d810683e08
Erik Bjorgan <6627466+domesticatedviking@users.noreply.github.com>
2019-03-03 22:50:53 -0800

    Delete skinnydip_appimage_workaround.sh

--- skinnydip_appimage_workaround.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-SCRIPT=`realpath $0`
-SCRIPTPATH=`dirname $SCRIPT`
-unset LD_LIBRARY_PATH
-unset PYTHONHOME
-unset PYTHONPATH
-unset PERLLIB
-exec /usr/bin/python2.7 $SCRIPTPATH/skinnydip.py $@
