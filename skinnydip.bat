873e76cbd306d8b9adb90aabd7c68ba5687a26fd
Erik Bjorgan <6627466+domesticatedviking@users.noreply.github.com>
2019-04-24 11:26:18 -0700

    Batch file for windows users
    Auto detection of python path added.

--- skinnydip.bat
+++ skinnydip.bat
@@ -1,5 +1,9 @@
-@echo off
-SET PYTHONPATH=C:\python27\python.exe
+@ echo off
+FOR /f %%p in ('where python') do SET PYTHONPATH=%%p
+REM If you wish to set your python location manually, do it in the line below
+REM You will also need to delete the "REM" from the beginning of the line.
+REM SET PYTHONPATH=C:\python27\python.exe
+ECHO Using python installation found at %PYTHONPATH%
 SET SCRIPTPATH=%~dp0
 %PYTHONPATH% %SCRIPTPATH%\skinnydip.py %1
 pause
